a196b286dfee2c9285fc943aed07fc51aa31fb2e
Mensire <mensirer@gmail.com>
2021-08-29 06:20:10 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<htdio.h>
+
+/**
+ * main -Executed first. Prints both lower and uppercase alphabets.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alpha;
+/*lowercase alphabet*/
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+/*uppercase alphabet*/
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	{
+	  putchar('\n');
+	}
+	return (0);
+}
