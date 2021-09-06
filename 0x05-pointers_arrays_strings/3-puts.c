aa4372095846c6b8a0f896de2bdc12d6b58c8184
Mensire <mensirer@gmail.com>
2021-09-06 13:04:29 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,12 @@
 /**
  * _puts - Prints a string to stdout.
  *
- *@str: The string that will be printed.
+ * @str: The string to be printed.
  */
 void _puts(char *str)
 {
-  while (*str)
-    _putchar(*str++);
+	while (*str)
+		_putchar(*str++);
 
-  _putchar('\n');
+	_putchar('\n');
 }
