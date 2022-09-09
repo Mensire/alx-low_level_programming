c798168a87b774ab2333140b1d7a80e6413fd8b5
Ruth <mensirer@gmail.com>
2022-09-09 11:21:42 -0400

    url

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Mensire/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /home/ruth/Desktop/data/alx-low_level_programming/0x18-dynamic_libraries/ https://github.com/Mensire/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/home/ruth/Desktop/data/alx-low_level_programming/0x18-dynamic_libraries/nrandom.so
